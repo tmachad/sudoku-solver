9c7c91c33c913db85573887ee7097304ae0ef1f0
Thomas Machado <TMachado@dacgroup.com>
2018-01-02 16:16:04 -0500

    Refactored puzzle solve() method for readability

--- src/models/puzzle.ts
+++ src/models/puzzle.ts
@@ -55,78 +55,63 @@ export class Puzzle {
     }
 
     public solve(): void {
-        let abort = false;
+        let startGuessing = false;
         const historyStack = new Array<HistoryRecord>();
-        while (this.unassigned.size > 0 && !abort) {
+        while (this.unassigned.size > 0) {
             let breakInner = false;
 
             // Solve all cells that have an unambiguous solution
             for (let row = 0; row < this.cells.length && !breakInner; row++) {
                 for (let col = 0; col < this.cells[row].length && !breakInner; col++) {
                     const cell = this.cells[row][col];
-                    // Found an unassigned cell that has an unambiguous solution
-                    if (cell.getValue() === 0 && cell.potentialValueCount() === 1) {
-                        const record = new HistoryRecord(cell);
-                        historyStack.push(record);
-                        const chosenValue = record.getFirstUntried();
-                        record.chosenValue = chosenValue.value;
-                        chosenValue.tried = true;
-                        cell.setValue(chosenValue.value);
-                        this.unassigned.delete(cell);
-                        breakInner = true;
-                    }
-                }
-            }
-            // If no unambiguous solutions are available, find a cell to guess a solution for
-            if (!breakInner) {
-                breakInner = false;
-                for (let row = 0; row < this.cells.length && !breakInner; row++) {
-                    for (let col = 0; col < this.cells[row].length && !breakInner; col++) {
-                        const cell = this.cells[row][col];
-                        // Found a cell that needs a solution
-                        if (cell.getValue() === 0) {
-                            // Cell has no potential values, so we've hit a dead end
-                            if (cell.potentialValueCount() === 0) {
-                                let rewind = true;
-                                while (rewind) {
-                                    const record = historyStack.pop();
-                                    if (record.untriedCount() === 0) {
-                                        record.cell.setValue(0);
-                                    } else {
-                                        record.cell.setValue(0);
-                                        const nextValue = record.getFirstUntried();
-                                        record.chosenValue = nextValue.value;
-                                        nextValue.tried = true;
-                                        record.cell.setValue(nextValue.value);
-                                        historyStack.push(record);
-                                        rewind = false;
-                                    }
+                    if (cell.getValue() === 0) {
+                        if (cell.potentialValueCount() === 0) {
+                            // A dead end has been reached, rewind history to the last branch
+                            let rewind = true;
+                            while (rewind) {
+                                const record = historyStack.pop();
+                                if (record.untriedCount() === 0) {
+                                    record.cell.setValue(0);
+                                    this.unassigned.add(record.cell);
+                                } else {
+                                    record.cell.setValue(0);
+                                    const nextValue = record.getFirstUntried();
+                                    record.chosenValue = nextValue.value;
+                                    nextValue.tried = true;
+                                    record.cell.setValue(nextValue.value);
+                                    historyStack.push(record);
+                                    rewind = false;
                                 }
-                            // Cell has potential values we can guess
-                            } else {
-                                const record = new HistoryRecord(cell);
-                                historyStack.push(record);
-                                const chosenValue = record.getFirstUntried();
-                                record.chosenValue = chosenValue.value;
-                                chosenValue.tried = true;
-                                cell.setValue(chosenValue.value);
-                                this.unassigned.delete(cell);
                             }
+                            startGuessing = false;  // reset this just in case it was set to true when rewind occurred
+                            breakInner = true;
+                        } else if (cell.potentialValueCount() === 1) {
+                            // Cell only has one possible value, so take that
+                            const record = new HistoryRecord(cell);
+                            historyStack.push(record);
+                            const chosenValue = record.getFirstUntried();
+                            record.chosenValue = chosenValue.value;
+                            chosenValue.tried = true;
+                            cell.setValue(chosenValue.value);
+                            this.unassigned.delete(cell);
+                            breakInner = true;
+                        } else if (startGuessing) {
+                            // Cell has multiple possible values, so try the first untried one
+                            const record = new HistoryRecord(cell);
+                            historyStack.push(record);
+                            const chosenValue = record.getFirstUntried();
+                            record.chosenValue = chosenValue.value;
+                            chosenValue.tried = true;
+                            cell.setValue(chosenValue.value);
+                            this.unassigned.delete(cell);
                             breakInner = true;
                         }
                     }
                 }
             }
 
-            if (!breakInner || abort) {
-                console.log("Hit a dead end. Time to go back up the stack!");
-                console.log(`${this.unassigned.size} unassigned cells left:`);
-                this.unassigned.forEach((cell) => {
-                    console.log(`[${cell.row.name}, ${cell.column.name}, ${cell.block.name}] ` +
-                        `value: ${cell.getValue()} pt: [${cell.getPotentialValues()}]`);
-                });
-                console.log("History stack:");
-                historyStack.forEach((history) => console.log(history.toString()));
+            if (!breakInner) {
+                startGuessing = true;
             }
         }
     }
